a8acf1f61da42729184b266ddb7c04c94a704f68
Jonathan Won <jabinx92@gmail.com>
2020-10-26 16:58:28 -0700

    tours

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -4,7 +4,56 @@ import Tours from './Tours';
 const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const deleteTour = (id) => {
+    const newTours = tours.filter((tour) => {
+     return tour.id !== id
+    })
+    setTours(newTours)
+  } 
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours)
+    } catch (error) {
+      setLoading(false);
+      console.log(error)
+    }
+
+  }
+
+  useEffect(()=> {
+    fetchTours();
+  }, [])
+
+  if(loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if(tours.length === 0) {
+    return <main>
+      <div className="title">
+      <h2>no tours left</h2>
+      <button className="btn" onClick={fetchTours}>refresh</button>
+
+      </div>
+    </main>
+  }
+  return <main>
+    <Tours tours={tours} deleteTour={deleteTour}/>
+  </main>;
+
+
 }
 
 export default App;
--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, deleteTour}) => {
+
+
+  return <section>
+    <div className="title">
+      <h2>our tours</h2>
+      <div className='underline'>
+
+      </div>
+    </div>
+    <div>
+      {tours.map((tour => {
+        return (
+          <Tour key={tour.id} {...tour} deleteTour={deleteTour}>
+
+          </Tour>
+        )
+      }))}
+    </div>
+  </section>;
 };
 
 export default Tours;
