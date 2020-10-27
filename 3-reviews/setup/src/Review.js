9166d2f493a526d8ef48e78ec8b7b3e93d853424
Jonathan Won <jabinx92@gmail.com>
2020-10-27 16:45:05 -0700

    accordion

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -3,7 +3,33 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index]
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={()=>{index !== 0 ? setIndex(index - 1) : setIndex(people.length - 1)}}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={()=>{index !== people.length - 1 ? setIndex(index + 1) : setIndex(0)}}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={()=>{setIndex(Math.floor(Math.random() * people.length))}}>
+        surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
