b4cbccd7247a1270a571d41a31a2ca9c5f988c6d
Jonathan Won <jabinx92@gmail.com>
2020-11-02 17:11:38 -0800

    shopping list

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,61 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [string, setString] = useState('');
+  const [items, setItems] = useState([]);
+  const [edit, setEdit] = useState(false)
+
+  const clearItems = () => {
+    setItems([])
+  }
+
+  const filterItems = (id) => {
+    const newItems = items.filter((item, index) => {
+      return index !== id
+    })
+
+    return setItems(newItems)
+  }
+
+  const editItem = (item) => {
+    setEdit(true);
+
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(string.length) {
+      setItems([...items, string])
+      setString('')
+      console.log(items)
+    } else {
+      alert('submission cannot be empty!')
+    }
+  }
+
+  return (
+  <div>
+  {/* {edit ? editItem: 'null'} */}
+  <h2>grocery list</h2>
+  <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" placeholder='e.g. eggs' value={string} onChange={event => setString(event.target.value)} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    
+    <div>
+      <List items={items} filterItems={filterItems} editItem={editItem}/>
+    </div>
+
+
+  <button onClick={clearItems}>Clear Items</button>
+  </div>
+  )
 }
 
 export default App
+
+
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items, filterItems, editItem}) => {
+    console.log(items)
+
+  const mapped = items.map((item, index) => {
+    return (
+      <div key={index}>
+        {item}
+        <button>
+          <FaEdit onClick={() => {editItem(index)}}/>
+        </button>
+        <button onClick={() => {filterItems(index)}}>
+          <FaTrash />
+        </button>
+      </div>
+    )
+  })
+
+  return (
+  <h2>{items.length > 0 ? mapped : ''}</h2>
+  )
 }
 
 export default List
